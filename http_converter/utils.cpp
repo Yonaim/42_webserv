47bca4223da87d79da8c90821d2340c48484417a
gitubanana <ptyolo666@gmail.com>
2023-05-10 16:00:07 +0900

    Feat: 파싱 관련 util 함수 추가 - split, strtrim, strBeforeSep, hasSpace

--- http_converter/utils.cpp
+++ http_converter/utils.cpp
@@ -1,12 +1,21 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
 
+bool hasSpace(const std::string &key)
+{
+	for (size_t i = 0; key[i]; ++i)
+		if (std::isspace(key[i]))
+			return (true);
+	return (false);
+}
+
 std::vector<std::string> *split(std::string const &s, char const c)
 {
 	std::vector<std::string> *words = new std::vector<std::string>;
-	int                       offset = 0;
-	int                       end_idx;
+	size_t                    offset = 0;
+	size_t                    end_idx;
 
 	while (offset < s.length())
 	{
@@ -23,8 +32,8 @@ std::vector<std::string> *split(std::string const &s, char const c)
 	return (words);
 }
 
-static size_t find_first_sep(std::string const &s, std::string const &sep,
-					  size_t offset)
+static size_t _findFirstSep(std::string const &s, std::string const &sep,
+							size_t offset)
 {
 	while (offset < s.length())
 	{
@@ -38,14 +47,14 @@ static size_t find_first_sep(std::string const &s, std::string const &sep,
 std::vector<std::string> *split(std::string const &s, std::string const &sep)
 {
 	std::vector<std::string> *words = new std::vector<std::string>;
-	int                       offset = 0;
-	int                       end_idx;
+	size_t                    offset = 0;
+	size_t                    end_idx;
 
 	while (offset < s.length())
 	{
 		if (sep.find(s[offset]) == std::string::npos)
 		{
-			end_idx = find_first_sep(s, sep, offset);
+			end_idx = _findFirstSep(s, sep, offset);
 			std::string word = s.substr(offset, end_idx - offset);
 			words->push_back(word);
 			offset = end_idx;
@@ -53,6 +62,34 @@ std::vector<std::string> *split(std::string const &s, std::string const &sep)
 		offset++;
 	}
 	return (words);
+}
+
+std::string strBeforeSep(const std::string &be_parsed, const std::string &sep,
+						 size_t &start)
+{
+	std::string result;
+	size_t      end;
+
+	end = be_parsed.find(sep, start);
+	if (end == std::string::npos)
+		return ("");
+	result = be_parsed.substr(start, end - start);
+	start = end + sep.size();
+	return (result);
+}
+
+std::string strtrim(std::string str, std::string charset)
+{
+	size_t start;
+	size_t offest;
+
+	start = 0;
+	while (str[start] && strchr(charset.c_str(), str[start]))
+		++start;
+	offest = start;
+	while (str[offest] && !strchr(charset.c_str(), str[offest]))
+		++offest;
+	return (str.substr(start, offest - start));
 }
 
 // int main(void)
--- http_converter/utils.hpp
+++ http_converter/utils.hpp
@@ -1,11 +1,14 @@
 #ifndef UTILS_HPP
-# define UTILS_HPP
+#define UTILS_HPP
 
 #include <string>
 #include <vector>
 
+bool                      hasSpace(const std::string &key);
+std::string               strtrim(std::string str, std::string charset);
 std::vector<std::string> *split(std::string const &s, char const c);
 std::vector<std::string> *split(std::string const &s, std::string const &sep);
-
+std::string strBeforeSep(const std::string &be_parsed, const std::string &sep,
+						 size_t &start);
 
 #endif
