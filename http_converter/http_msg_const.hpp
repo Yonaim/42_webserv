d9bebf04356a3d0b1ab0c2a204f5dc5f01a5ece5
gitubanana <ptyolo666@gmail.com>
2023-05-10 21:49:07 +0900

    Feat: literal 변수명 바꿈

--- http_converter/http_msg_const.hpp
+++ http_converter/http_msg_const.hpp
@@ -7,10 +7,9 @@
 
 namespace http_msg
 {
-typedef std::string                                str_t;
-typedef std::vector<std::string>                   str_vec_t;
+typedef std::string                str_t;
+typedef std::vector<std::string>   str_vec_t;
 typedef std::map<str_t, str_vec_t> str_map_t;
-typedef std::map<str_t, str_vec_t>::const_iterator map_iter_t;
 
 const std::string CRLF = "\r\n";
 const std::string SP = " ";
@@ -20,7 +19,7 @@ const int         SP_LEN = 1;
 namespace method
 {
 const int         kCount = 3;
-const std::string kTypeStr[kCount] = {"GET", "POST", "DELET"};
+const std::string kTypeStr[kCount] = {"GET", "POST", "DELETE"};
 
 enum e_type
 {
@@ -32,13 +31,13 @@ enum e_type
 
 enum e_errcode
 {
-	ERR_START_LINE_PARSING,
-	ERR_HEADER_PARSING,
-	ERR_BODY_PARSING
+	kErrStartLineParsing,
+	kErrHeaderParsing,
+	kErrBodyParsing
 };
 
-const std::string kErrMsg[3] = {"start line parsing failed", "header parsing failed",
-						  "body parsing failed"};
+const std::string kErrMsg[3] = {"start line parsing failed",
+								"header parsing failed", "body parsing failed"};
 } // namespace http_msg
 
 #endif
