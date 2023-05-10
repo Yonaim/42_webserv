06e477aea6699aa425fa9962b8675de1d2fdbab1
gitubanana <ptyolo666@gmail.com>
2023-05-10 21:53:17 +0900

    Feat: 무엇을 만들어야 할지 내용 추가

--- http_converter/HttpRes.cpp
+++ http_converter/HttpRes.cpp
@@ -4,7 +4,7 @@ using http_msg::HttpRes;
 using http_msg::str_t;
 using http_msg::str_vec_t;
 
-HttpRes::HttpRes(int status)
+HttpRes::HttpRes(int status) : _version("HTTP/1.1")
 {
 }
 
--- http_converter/HttpRes.hpp
+++ http_converter/HttpRes.hpp
@@ -5,6 +5,17 @@
 
 // 시작줄 : [HTTP 버전] [상태 코드] [사유 구절] # 공백으로 띄워진다.
 // 헤더, 빈 줄, 엔티티 본문이 온다.
+/*
+	HTTP/1.1 200 OK
+	Server: nginx/1.23.3
+	Date: Wed, 10 May 2023 07:35:44 GMT
+	Content-Type: text/html
+	Content-Length: 611
+	Last-Modified: Wed, 03 May 2023 07:10:36 GMT
+	Connection: keep-alive
+	ETag: "645208ec-263"
+	Accept-Ranges: bytes
+*/
 
 namespace http_msg
 {
@@ -16,14 +27,19 @@ class HttpRes
 	str_t toString(void);
 
 	void setHeaderField(std::string const &key, std::string const &val);
-	// server
-	// Content-Length
+	// Server
+	// Date
 	// Content-Type
+	// Content-Length
+	// Last-Modified
+	// Connection
+	// ETag
+	// Accept-Ranges
 
   private:
 	str_t _dest;
 
-	const str_t                _version = "HTTP/1.1";
+	const str_t                _version;
 	int                        _status;
 	str_t                      _comment;
 	str_t                      _body; // 실제 엔티티 본문
