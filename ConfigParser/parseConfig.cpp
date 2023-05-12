a47be3e42f7f4707d0ebba5d5dfaa3d19ed31770
Donghyeon Lee <earthicko@gmail.com>
2023-05-12 16:26:41 +0900

    Feat: 재귀하향식 parseConfig 추가

---
+++ ConfigParser/parseConfig.cpp
@@ -0,0 +1,80 @@
+#include "ConfigDirective.hpp"
+#include "parseConfigInternal.hpp"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+const char comment_marker = '#';
+
+void splitIntoTokens(std::vector<std::string> &tokens,
+					 const std::string &content);
+
+ParsingFailException::ParsingFailException(const std::string &why)
+	: std::runtime_error(why)
+{
+}
+
+std::string loadFileFromPath(const std::string &path)
+{
+	std::ifstream infile;
+	std::stringstream buffer;
+
+	infile.open(path, std::ios::in);
+	if (!infile.good())
+		throw(std::runtime_error("Error while opening file from " + path));
+	buffer << infile.rdbuf();
+	infile.close();
+	return (buffer.str());
+}
+
+void splitContentIntoLines(std::vector<std::string> &lines,
+						   const std::string &content)
+{
+	std::stringstream buffer;
+	buffer.str(content);
+	while (!buffer.eof())
+	{
+		std::string line;
+		std::getline(buffer, line);
+		lines.push_back(line);
+	}
+}
+
+void trimComments(std::string &content)
+{
+	std::vector<std::string> lines;
+	splitContentIntoLines(lines, content);
+	content = "";
+	for (std::vector<std::string>::iterator line = lines.begin();
+		 line != lines.end(); line++)
+	{
+		size_t comment_position = line->find(comment_marker);
+		if (comment_position == std::string::npos)
+			content += *line;
+		else
+			content += line->substr(0, comment_position);
+		content += "\n";
+	}
+}
+
+std::vector<std::string> tokenizeConfig(std::string &content)
+{
+	trimComments(content);
+	std::vector<std::string> tokens;
+	splitIntoTokens(tokens, content);
+	return (tokens);
+}
+
+ConfigContext parseConfig(const std::string &path)
+{
+	std::string file_content = loadFileFromPath(path);
+	std::vector<std::string> tokens = tokenizeConfig(file_content);
+	std::vector<std::string>::iterator cursor = tokens.begin();
+	std::vector<std::string> parameters;
+	std::vector<ConfigDirective *> directives;
+	while (cursor != tokens.end())
+		directives.push_back(parseAvailableConfigDirective(cursor, tokens));
+	return (ConfigContext("root", parameters, directives));
+}
---
+++ ConfigParser/recursiveParser.cpp
@@ -0,0 +1,91 @@
+#include "ConfigDirective.hpp"
+#include "parseConfigInternal.hpp"
+#include <string>
+
+bool isSpecialToken(const std::string &token)
+{
+	for (size_t i = 0; i < sizeof(special_tokens) / sizeof(std::string); i++)
+	{
+		if (special_tokens[i] == token)
+			return (true);
+	}
+	return (false);
+}
+
+ConfigDirective parseConfigDirective(std::vector<std::string>::iterator &cursor,
+									 const std::vector<std::string> &tokens)
+{
+	const std::string &name = *cursor;
+	std::vector<std::string> parameters;
+	std::vector<std::string>::iterator cursor_end = cursor;
+	while (true)
+	{
+		cursor_end++;
+		if (isSpecialToken(*cursor_end) || cursor_end == tokens.end())
+			break;
+		parameters.push_back(*cursor_end);
+	}
+	if (cursor_end == tokens.end())
+		throw(ParsingFailException("Reached end of tokens without semicolon."));
+	if (*cursor_end != token_semicolon)
+		throw(
+			ParsingFailException("Unexpected token \"" + *cursor_end + "\"."));
+	if (cursor_end == cursor)
+		throw(ParsingFailException("Not enough tokens to parse."));
+	cursor = cursor_end;
+	cursor++;
+	return (ConfigDirective(name, parameters));
+}
+
+ConfigContext parseConfigContext(std::vector<std::string>::iterator &cursor,
+								 const std::vector<std::string> &tokens)
+{
+	const std::string &name = *cursor;
+	std::vector<std::string> parameters;
+	std::vector<ConfigDirective *> directives;
+	std::vector<std::string>::iterator cursor_end = cursor;
+	while (true)
+	{
+		cursor_end++;
+		if (isSpecialToken(*cursor_end) || cursor_end == tokens.end())
+			break;
+		parameters.push_back(*cursor_end);
+	}
+	if (cursor_end == tokens.end())
+		throw(ParsingFailException("Reached end of tokens without braces."));
+	if (*cursor_end != token_brace_open)
+		throw(
+			ParsingFailException("Unexpected token \"" + *cursor_end + "\"."));
+	if (cursor_end == cursor)
+		throw(ParsingFailException("Not enough tokens to parse."));
+	cursor_end++;
+	while (true)
+	{
+		if (isSpecialToken(*cursor_end) || cursor_end == tokens.end())
+			break;
+		directives.push_back(parseAvailableConfigDirective(cursor_end, tokens));
+	}
+	if (cursor_end == tokens.end())
+		throw(ParsingFailException("Reached end of tokens without braces."));
+	if (*cursor_end != token_brace_close)
+		throw(
+			ParsingFailException("Unexpected token \"" + *cursor_end + "\"."));
+	cursor = cursor_end;
+	cursor++;
+	return (ConfigContext(name, parameters, directives));
+}
+
+ConfigDirective *parseAvailableConfigDirective(
+	std::vector<std::string>::iterator &cursor,
+	const std::vector<std::string> &tokens)
+{
+	try
+	{
+		return (new ConfigDirective(parseConfigDirective(cursor, tokens)));
+	}
+	catch (const std::exception &e)
+	{
+		(void)e;
+	}
+	return (new ConfigContext(parseConfigContext(cursor, tokens)));
+}
