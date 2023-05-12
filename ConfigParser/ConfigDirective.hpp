830435107982f376007f79d4fa9d2a677304ee80
Donghyeon Lee <earthicko@gmail.com>
2023-05-12 14:50:28 +0900

    Feat: ConfigDirective.hpp 수정
    - 접근 제한자 protected로
    - getter 추가
    - is_context 추가

--- ConfigParser/ConfigDirective.hpp
+++ ConfigParser/ConfigDirective.hpp
@@ -6,31 +6,39 @@
 
 class ConfigDirective
 {
-  private:
+  protected:
 	std::string _name;
 	std::vector<std::string> _parameters;
-	ConfigDirective(void);
+	bool is_context;
 
   public:
+	ConfigDirective(const std::string name = "");
 	ConfigDirective(const std::string &name,
 					const std::vector<std::string> &parameters);
 	~ConfigDirective();
 	ConfigDirective(const ConfigDirective &orig);
 	ConfigDirective &operator=(const ConfigDirective &orig);
+
+	const std::string &name(void) const;
+	const std::string &parameter(size_t index) const;
+	size_t nParameters(void) const;
 };
 
 class ConfigContext : public ConfigDirective
 {
   private:
 	std::vector<ConfigDirective *> _directives;
-	ConfigContext(void);
 
   public:
+	ConfigContext(const std::string name = "");
 	ConfigContext(const std::string &name,
-				  const std::vector<ConfigDirective> &directives);
+				  const std::vector<ConfigDirective *> &directives);
 	~ConfigContext();
 	ConfigContext(const ConfigContext &orig);
 	ConfigContext &operator=(const ConfigContext &orig);
+
+	const ConfigDirective &directive(size_t index) const;
+	size_t nDirectives(void) const;
 };
 
 #endif
