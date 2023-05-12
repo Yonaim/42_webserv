fd6ce8fbbea1aff602ff76d95469fd187ddffdcb
yonaCluster <yonaimyo@gmail.com>
2023-05-12 13:19:02 +0900

    Feat: HTTPServer 클래스 정의 추가

---
+++ HTTPServer/HTTPServer.hpp
@@ -0,0 +1,55 @@
+#ifndef HTTPSERVER_HPP
+#define HTTPSERVER_HPP
+
+#include "../AsyncIOProcessor/AsyncTCPIOProcessor.hpp"
+#include "../ConfigParser/ConfigDirective.hpp"
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+enum e_http_method
+{
+	GET = 0,
+	HEAD,
+	POST,
+	PUT,
+	DELETE,
+	CONNECT,
+	OPTIONS,
+	TRACE
+};
+
+class HTTPServer
+{
+  private:
+	class HTTPLocation
+	{
+	  private:
+		bool has_index;
+		bool do_redirection;
+		bool autoindex;
+		std::string root;
+		std::string index;
+		std::pair<int, std::string> redirection;
+		std::unordered_set<int> allowed_methods;
+
+	  public:
+		HTTPLocation();
+		~HTTPLocation();
+		HTTPLocation(const HTTPLocation &orig);
+		HTTPLocation &operator=(const HTTPLocation &orig);
+	};
+
+	int port;
+	std::string server_name;
+	std::map<int, std::string> error_pages;
+	std::vector<std::string, HTTPLocation> locations;
+
+  public:
+	HTTPServer(const ConfigContext &server_context);
+	~HTTPServer();
+	HTTPServer(const HTTPServer &orig);
+	HTTPServer &operator=(const HTTPServer &orig);
+};
+
+#endif
