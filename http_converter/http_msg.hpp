c3246f96c6ae5ffda338893d76b386b08e80be5d
hey-uu <khy2714@gmail.com>
2023-05-15 15:53:39 +0900

    Refactor: http_msg 필요없는 literal 정의 삭제

--- http_converter/http_msg.hpp
+++ http_converter/http_msg.hpp
@@ -30,38 +30,6 @@ enum eMethodType
 const int         kMethodCount = 3;
 const std::string kMethodStr[kCount] = {"GET", "POST", "DELETE"};
 
-// header fields
-const std::string kGeneralHeaderFields[]
-	= {"cache-control",     "connection", "date", "pragma", "trailer",
-	   "transfer-encoding", "upgrade",    "via",  "warning"};
-const std::string kRequestHeaderFields[] = {"accept",
-											"accept-charset",
-											"accept-encoding",
-											"accept-language",
-											"authorization",
-											"expect",
-											"from",
-											"host",
-											"if-match",
-											"if-modified-since",
-											"if-none-match",
-											"if-range",
-											"if-unmodified-since",
-											"max-forwards",
-											"proxy-authorization",
-											"range",
-											"referer",
-											"te",
-											"user-agent"};
-const std::string kResponseHeaderFields[] = {
-	"accept-ranges", "age",    "etag", "location",        "proxy-authenticate",
-	"retry-after",   "server", "vary", "www-authenticate"};
-const std::string kEntityHeaderFields[]
-	= {"allow",          "content-encoding", "content-language",
-	   "content-length", "content-location", "content-md5",
-	   "content-range",  "content-type",     "expires",
-	   "last-modified",  "extension-header"};
-
 // error message
 const std::string kErrStartLineParsing = "failed to parse the request line.";
 const std::string kErrHeaderParsing = "failed to parse the header fields.";
