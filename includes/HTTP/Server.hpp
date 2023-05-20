ec0cc4ae5ff40679953691f844bfee7a45762f74
gitubanana <ptyolo666@gmail.com>
2023-05-20 17:55:51 +0900

    Fix: Server.hpp RequestHandler, Location,
     ServerException 변경 사항 반영

--- includes/HTTP/Server.hpp
+++ includes/HTTP/Server.hpp
@@ -15,43 +15,18 @@ namespace HTTP
 {
 class Server
 {
+  public:
+	class RequestHandler;
+
   private:
-	class Location
-	{
-	  private:
-		bool _has_index;
-		bool _do_redirection;
-		bool _autoindex;
-		std::string _path;
-		std::string _root;
-		std::vector<std::string> _index;
-		std::pair<int, std::string> _redirection;
-		std::set<int> _allowed_methods;
-
-	  public:
-		Location();
-		Location(const ConfigContext &location_context);
-		~Location();
-		Location(const Location &orig);
-		Location &operator=(const Location &orig);
-
-		void parseDirectiveRoot(const ConfigContext &location_context);
-		void parseDirectiveLimitExcept(const ConfigContext &location_context);
-		void parseDirectiveReturn(const ConfigContext &location_context);
-		void parseDirectiveAutoIndex(const ConfigContext &location_context);
-		void parseDirectiveIndex(const ConfigContext &location_context);
-		const std::string &getPath(void) const;
-
-		// 추가
-		const std::string &getRoot(void) const;
-		bool isAllowedMethod(int method);
-	};
+	class Location;
+	class ServerException;
 
 	int _port;
 	std::set<std::string> _server_name;
 	std::map<int, std::string> _error_pages;
 	std::map<std::string, Location> _locations;
-	std::map<int, std::vector<RequestHandler> > _request_handlers;
+	std::map<int, std::queue<RequestHandler *> > _request_handlers;
 	std::map<int, std::queue<Response> > _output_queue;
 
 	void parseDirectiveListen(const ConfigContext &server_context);
@@ -60,18 +35,6 @@ class Server
 	void parseDirectiveLocation(const ConfigContext &server_context);
 	void ensureClientConnected(int client_fd);
 	static bool isValidStatusCode(const int &status_code);
-
-	// 추가
-	void getMethodHandler(HTTP::Request &request, HTTP::Response &response,
-						  const std::string &resource_path);
-	void headMethodHandler(HTTP::Request &request, HTTP::Response &response,
-						   const std::string &resource_path);
-	void postMethodHandler(HTTP::Request &request, HTTP::Response &response,
-						   const std::string &resource_path);
-	void deleteMethodHandler(HTTP::Request &request, HTTP::Response &response,
-							 const std::string &resource_path);
-	std::string processURI(const HTTP::Server::Location &location,
-						   std::string uri);
 
   public:
 	Server(const ConfigContext &server_context);
@@ -89,10 +52,14 @@ class Server
 	void disconnect(int client_fd);
 
 	// 추가
-	void methodHandler(HTTP::Request &Request, int fd);
 	const Location &getLocation(const std::string &location);
 	void setErrorPage(HTTP::Response &response, int status_code);
+	std::string getResourcePath(const Request &req) const;
 };
 } // namespace HTTP
+
+#include "HTTP/ServerException.hpp"
+#include "HTTP/ServerLocation.hpp"
+#include "RequestHandler.hpp"
 
 #endif
---
+++ includes/HTTP/ServerLocation.hpp
@@ -0,0 +1,39 @@
+#ifndef SERVERLOCATION_HPP
+#define SERVERLOCATION_HPP
+
+#include "Server.hpp"
+
+namespace HTTP
+{
+class Server::Location
+{
+  private:
+	bool _has_index;
+	bool _do_redirection;
+	bool _autoindex;
+	std::string _path;
+	std::string _root;
+	std::vector<std::string> _index;
+	std::pair<int, std::string> _redirection;
+	std::set<int> _allowed_methods;
+
+  public:
+	Location();
+	Location(const ConfigContext &location_context);
+	~Location();
+	Location(const Location &orig);
+	Location &operator=(const Location &orig);
+
+	void parseDirectiveRoot(const ConfigContext &location_context);
+	void parseDirectiveLimitExcept(const ConfigContext &location_context);
+	void parseDirectiveReturn(const ConfigContext &location_context);
+	void parseDirectiveAutoIndex(const ConfigContext &location_context);
+	void parseDirectiveIndex(const ConfigContext &location_context);
+	const std::string &getPath(void) const;
+
+	// 추가
+	const std::string &getRoot(void) const;
+	bool isAllowedMethod(int method);
+};
+} // namespace HTTP
+#endif
