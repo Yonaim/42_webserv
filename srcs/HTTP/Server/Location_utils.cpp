9ccbfe96fd83eb988f2ab5b659b0d04fde707981
gitubanana <ptyolo666@gmail.com>
2023-05-20 17:56:28 +0900

    Feat: Server 클래스의 getResourcePath 멤버 함수 정의 추가

--- srcs/HTTP/Server/Location_utils.cpp
+++ srcs/HTTP/Server/Location_utils.cpp
@@ -1,6 +1,6 @@
 #include "../const_values.hpp"
-#include "HTTP/Server.hpp"
-#include "HTTPServerException.hpp"
+#include "HTTP/ServerException.hpp"
+#include "HTTP/ServerLocation.hpp"
 #include <iostream>
 
 const std::string &HTTP::Server::Location::getRoot(void) const
@@ -27,4 +27,13 @@ const HTTP::Server::Location &HTTP::Server::getLocation(
 		throw(ServerException(404));
 	}
 	return (iter->second);
+}
+
+std::string HTTP::Server::getResourcePath(const Request &req) const
+{
+	const int method = req.getMethod();
+	const std::string uri_path = req.getURIPath();
+	const Location location = getLocation(uri_path);
+
+	return (location.getRoot() + uri_path);
 }
