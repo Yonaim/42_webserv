3800ab07ba65e0a41bc489810ecb7333c43cbe12
gitubanana <ptyolo666@gmail.com>
2023-05-20 18:01:13 +0900

    Fix: Server 소스파일 RequestHandler 수정 사항 반영

--- srcs/HTTP/Server/Method.cpp
+++ srcs/HTTP/Server/Method.cpp
@@ -1,6 +1,6 @@
 #include "../const_values.hpp"
 #include "HTTP/Server.hpp"
-#include "HTTPServerException.hpp"
+#include "HTTP/ServerException.hpp"
 #include "async/FileIOProcessor.hpp"
 #include "utils.hpp"
 #include <fcntl.h>
@@ -8,12 +8,6 @@
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
-
-// read
-// parse
-// process
-// response
-//
 
 void HTTP::Server::setErrorPage(HTTP::Response &response, int status_code)
 {
@@ -30,53 +24,8 @@ void HTTP::Server::setErrorPage(HTTP::Response &response, int status_code)
 	// TODO: error_page를 여는 것을 실패했을때 어떻게 처리할지 결정할 것
 	if (fd < 0)
 		;
-	async::FileReader reader(fd);
 
-	reader.task();
-}
-
-void HTTP::Server::methodHandler(HTTP::Request &request, int fd)
-{
-	Response response;
-
-	try
-	{
-		const int method = request.getMethod();
-		const std::string uri_path = request.getURIPath();
-		const Location location = getLocation(uri_path);
-		const std::string resource_path = location.getRoot() + uri_path;
-
-		// TODO:
-		// server 설정에서 주어진 method가 허용 method에 속하는지 체크하는 부분
-		switch (method)
-		{
-		case METHOD_GET:
-			getMethodHandler(request, response);
-			break;
-		case METHOD_HEAD:
-			headMethodHandler(request, response);
-			break;
-		case METHOD_POST:
-			postMethodHandler(request, response);
-			break;
-		case METHOD_DELETE:
-			deleteMethodHandler(request, response);
-			break;
-		default:
-			throw(ServerException(501));
-			// TODO:
-			// 501 not implemented 에러 반환
-			// 이전에 파싱한 부분 약간 수정해야할듯
-			// 존재하지 않는 메소드는 METHOD_NONE으로 저장해놓고 여기서 에러
-			// 핸들링하도록
-		}
-	}
-	catch (HTTP::ServerException &e)
-	{
-		std::cout << e.what() << std::endl;
-		setErrorPage(response, e.getStatusCode());
-	}
-	_output_queue[fd].push(response);
+	_reader.task();
 }
 
 void HTTP::Server::setErrorPage(HTTP::Response &response, int status_code)
@@ -94,104 +43,4 @@ void HTTP::Server::setErrorPage(HTTP::Response &response, int status_code)
 	std::string content;
 	response.setBody(content);
 	response.setStatus(status_code);
-}
-
-// uri = <스킴>://<사용자
-// 이름>:<비밀번호>@<호스트>:<포트>/<경로>;<파라미터>?<질의>#<프래그먼트>
-
-void HTTP::Server::getMethodHandler(HTTP::Request &request,
-									HTTP::Response &response)
-{
-	// TODO:
-	const std::string resource_path;
-	const std::ifstream resource(resource_path, std::ios::binary);
-	std::stringstream buffer;
-
-	// 해당 파일 전체를 아직 다 안읽은 상황
-	// again을 호출하든지...
-	// 다 읽었으면 뒤에를 실행
-
-	if (!resource.is_open())
-	{
-		// 404 에러 페이지는 디폴트로 설정되어있다는 가정하에
-		response.setStatus(404);
-		response.setBody(_error_pages.find(404)->second);
-		return;
-	}
-	buffer << resource.rdbuf();
-	std::string content = buffer.str();
-
-	// 성공적으로 읽었음을 설정
-	response.setStatus(200);
-	response.setBody(content);
-	response.setContentLength(content.length());
-	response.setContentType(resource_path);
-}
-
-void HTTP::Server::headMethodHandler(HTTP::Request &request,
-									 HTTP::Response &response)
-{
-	// TODO:
-	// resource path 구하는 로직 필요
-	// const std::string resource_path = processURI(request.getURIPath());
-	const std::string resource_path;
-
-	const std::ifstream resource(resource_path, std::ios::binary);
-	std::stringstream buffer;
-
-	if (!resource.is_open())
-	{
-		setErrorPage(response, 404);
-		return;
-	}
-	buffer << resource.rdbuf();
-	std::string content = buffer.str();
-
-	// 성공적으로 읽었음을 설정
-	response.setStatus(200);
-	response.setContentLength(content.length());
-	response.setContentType(resource_path);
-}
-
-// POST 메소드
-//		: CGI
-//		: 리소스 생성
-//		: 리소스에 내용 추가
-void HTTP::Server::postMethodHandler(HTTP::Request &request,
-									 HTTP::Response &response)
-{
-	// const std::string resource_path
-	// 	= processURI(request.getURI()); // resource path 구하는 로직 필요
-
-	std::ofstream resource(resource_path, std::ios::app);
-
-	// if (/* cgi가 아니면 */)
-	if (!resource.is_open())
-	{
-		response.setStatus(404);
-		response.setBody(_error_pages.find(404)->second);
-	}
-	else
-	{
-		response.setStatus(200);
-		response.setValue("Location", resource_path);
-		resource << request.getBody();
-	}
-	// else
-}
-
-// 해당되는 URI 파일을 지운다.
-void HTTP::Server::deleteMethodHandler(HTTP::Request &request,
-									   HTTP::Response &response)
-{
-	// const std::string resource_path
-	// 	= processURI(request.getURI()); // resource path 구하는 로직 필요
-
-	if (unlink(resource_path.c_str()) == -1)
-	{
-		response.setStatus(404);
-		response.setBody(_error_pages.find(404)->second);
-	}
-	else
-		response.setStatus(200);
 }
--- srcs/HTTP/Server/Server.cpp
+++ srcs/HTTP/Server/Server.cpp
@@ -1,5 +1,6 @@
-#include "ConfigDirective.hpp"
+#include "../const_values.hpp"
 #include "HTTP/Server.hpp"
+#include "ConfigDirective.hpp"
 #include <cctype>
 #include <iostream>
 #include <sstream>
@@ -38,7 +39,26 @@ int HTTP::Server::getPort(void) const
 
 void HTTP::Server::task(void)
 {
-	std::cout << "Unimplemented stub of " << __func__ << std::endl;
+	for (std::map<int, std::queue<RequestHandler *> >::iterator it
+		 = _request_handlers.begin();
+		 it != _request_handlers.end(); it++)
+	{
+		int client_fd = it->first;
+		std::queue<RequestHandler *> &handlers = it->second;
+
+		int rc = handlers.front()->task();
+		if (rc == RequestHandler::RESPONSE_STATUS_OK)
+		{
+			_output_queue[client_fd].push(handlers.front()->retrieve());
+			handlers.pop();
+		}
+		else if (rc == RequestHandler::RESPONSE_STATUS_AGAIN)
+			continue;
+		else
+		{
+			// TODO: 예외 처리
+		}
+	}
 }
 
 bool HTTP::Server::isForMe(const HTTP::Request &request)
@@ -60,12 +80,36 @@ void HTTP::Server::ensureClientConnected(int client_fd)
 
 void HTTP::Server::registerRequest(int client_fd, const Request &request)
 {
-	if (_input_queue.find(client_fd) == _input_queue.end())
+	if (_request_handlers.find(client_fd) == _request_handlers.end())
 	{
-		_input_queue[client_fd] = std::queue<HTTP::Request>();
-		_output_queue[client_fd] = std::queue<HTTP::Response>();
+		_request_handlers[client_fd] = std::queue<RequestHandler *>();
+		_output_queue[client_fd] = std::queue<Response>();
 	}
-	_input_queue[client_fd].push(request);
+
+	RequestHandler *handler;
+	switch (request.getMethod())
+	{
+	case METHOD_GET:
+		handler = new RequestGetHandler(request);
+		break;
+	case METHOD_HEAD:
+		handler = new RequestHeadHandler(request);
+		break;
+	case METHOD_POST:
+		handler = new RequestPostHandler(request);
+		break;
+	case METHOD_DELETE:
+		handler = new RequestDeleteHandler(request);
+		break;
+	default:
+		throw(ServerException(501));
+		// TODO:
+		// 501 not implemented 에러 반환
+		// 이전에 파싱한 부분 약간 수정해야할듯
+		// 존재하지 않는 메소드는 METHOD_NONE으로 저장해놓고 여기서 에러
+		// 핸들링하도록
+	}
+	_request_handlers[client_fd].push(handler);
 }
 
 HTTP::Response HTTP::Server::retrieveResponse(int client_fd)
@@ -103,6 +147,6 @@ bool HTTP::Server::hasResponses(int client_fd)
 void HTTP::Server::disconnect(int client_fd)
 {
 	ensureClientConnected(client_fd);
-	_input_queue.erase(client_fd);
+	_request_handlers.erase(client_fd);
 	_output_queue.erase(client_fd);
 }
