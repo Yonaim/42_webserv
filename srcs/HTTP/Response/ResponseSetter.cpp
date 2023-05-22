6a67b85dc2e1f007626c7402213d34b4bf9cf176
gitubanana <ptyolo666@gmail.com>
2023-05-22 13:16:15 +0900

    Fix: HTTP/Response 멤버 함수 수정 및 삭제

--- srcs/HTTP/Response/ResponseSetter.cpp
+++ srcs/HTTP/Response/ResponseSetter.cpp
@@ -1,42 +1,34 @@
 #include "../const_values.hpp"
 #include "HTTP/Response.hpp"
 #include "utils/string.hpp"
+#include <ctime>
 #include <iostream>
 
-void HTTP::Response::setDefaultValue(void)
+void HTTP::Response::setValue(const std::string &name, const std::string &value)
 {
-	setConnection();
-	setContentLength();
-}
-
-void HTTP::Response::setValue(const std::string &key, const std::string &val)
-{
-	(void)key;
-	(void)val;
-	std::cout << "Unimplemented stub of " << __func__ << std::endl;
+	_header.insert(name, value);
 }
 
 void HTTP::Response::setDate(void)
 {
-	std::cout << "Unimplemented stub of " << __func__ << std::endl;
-	// time_t cur_time = time(NULL);
-	// tm *tm_gmt = gmtime(&cur_time);
+	time_t cur_time = time(NULL);
+	tm *tm_gmt = gmtime(&cur_time);
 
-	// static const std::string month[]
-	// 	= {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
-	// 	   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-	// static const std::string day_name[]
-	// 	= {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	static const std::string month[]
+		= {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+		   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+	static const std::string day_name[]
+		= {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
-	// std::string date(day_name[tm_gmt->tm_wday] + ", ");
+	std::string date(day_name[tm_gmt->tm_wday] + ", ");
 
-	// date.append(toStr(tm_gmt->tm_mday / 10, tm_gmt->tm_mday % 10) + " ");
-	// date.append(month[tm_gmt->tm_mon] + " ");
-	// date.append(toStr(tm_gmt->tm_year + 1900) + " ");
-	// date.append(toStr(tm_gmt->tm_hour / 10, tm_gmt->tm_hour % 10) + ":");
-	// date.append(toStr(tm_gmt->tm_min / 10, tm_gmt->tm_min % 10) + ":");
-	// date.append(toStr(tm_gmt->tm_sec / 10, tm_gmt->tm_sec % 10) + " GMT");
-	// setValue("Date", date);
+	date.append(toStr(tm_gmt->tm_mday / 10, tm_gmt->tm_mday % 10) + " ");
+	date.append(month[tm_gmt->tm_mon] + " ");
+	date.append(toStr(tm_gmt->tm_year + 1900) + " ");
+	date.append(toStr(tm_gmt->tm_hour / 10, tm_gmt->tm_hour % 10) + ":");
+	date.append(toStr(tm_gmt->tm_min / 10, tm_gmt->tm_min % 10) + ":");
+	date.append(toStr(tm_gmt->tm_sec / 10, tm_gmt->tm_sec % 10) + " GMT");
+	setValue("Date", date);
 }
 
 void HTTP::Response::setStatus(int status_code)
@@ -64,12 +56,27 @@ void HTTP::Response::setStatus(int status_code)
 	_reason_phrase = iter->second;
 }
 
-void HTTP::Response::setConnection(void)
+void HTTP::Response::setContent(const std::string &content, const std::string &file_path)
 {
-	// if (_request->hasHeaderVal("Connection", "close") == true)
-	// 	setValue("Connection", "close");
-	// else
-	// 	setValue("Connection", "keep-alive");
+	setContentType(file_path);
+	setContentLength(content.length());
+	setBody(content);
+}
+
+void HTTP::Response::setContentType(const std::string &file_path)
+{
+	size_t separator_idx = file_path.find_last_of('.');
+	if (separator_idx != std::string::npos)
+	{
+		const std::string extension = file_path.substr(separator_idx + 1);
+		const std::map<std::string, std::string>::const_iterator iter
+			= MIME_TYPE.find(extension);
+		if (iter == MIME_TYPE.end())
+			// default mime-type
+			setValue("Content-Type", "application/octet-stream");
+		else
+			setValue("Content-Type", iter->second);
+	}
 }
 
 void HTTP::Response::setContentLength(void)
@@ -84,23 +91,15 @@ void HTTP::Response::setContentLength(size_t length)
 		setValue("Content-Length", uintToStr(length));
 }
 
+void HTTP::Response::setConnection(bool is_persistent)
+{
+	if (is_persistent)
+		setValue("Connection", "keep-alive");
+	else
+		setValue("Connection", "close");
+}
+
 void HTTP::Response::setBody(const std::string &body)
 {
 	_body = body;
-}
-
-void HTTP::Response::setContentType(const std::string &file_path)
-{
-	size_t separator_idx = file_path.find_last_of('.');
-	if (separator_idx != std::string::npos)
-	{
-		const std::string extension = file_path.substr(separator_idx + 1);
-		const std::map<std::string, std::string>::const_iterator iter
-			= MIME_TYPE.find(extension);
-		if (iter == MIME_TYPE.end())
-			setValue("Content-Type",
-					 "application/octet-stream"); // default mime-type
-		else
-			setValue("Content-Type", iter->second);
-	}
 }
