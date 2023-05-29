6fe414883520303a9eaad710b9a1e814fdd50a32
yonaCluster <yonaimyo@gmail.com>
2023-05-29 14:14:17 +0900

    Fix: CGI Response 프로젝트에 맞춰 구성 변경

--- includes/CGI/Response.hpp
+++ includes/CGI/Response.hpp
@@ -11,20 +11,17 @@ class Response
   private:
 	int _type;
 	std::string _content_type;
-	std::string _location;
 	int _status;
-	std::vector<std::string> _protocol_specific_header_fields;
-	std::vector<std::string> _extension_header_fields;
-	std::string _message_body;
+	std::string _response_body;
 
   public:
-	enum _response_type_e
-	{
-		RESPONSE_DOCUMENT,
-		RESPONSE_LOCAL_REDIR,
-		RESPONSE_CLIENT_REDIR,
-		RESPONSE_CLIENT_REDIRDOC
-	};
+	// enum _response_type_e
+	// {
+	// 	RESPONSE_DOCUMENT,
+	// 	RESPONSE_LOCAL_REDIR,
+	// 	RESPONSE_CLIENT_REDIR,
+	// 	RESPONSE_CLIENT_REDIRDOC
+	// };
 	Response();
 	~Response();
 	Response(const Response &orig);
