01ddbf502ab7d40bc9d8aae0f89b13074bb2a4cc
hey-uu <khy2714@gmail.com>
2023-05-30 13:00:18 +0900

    Test: CGI/RequestHandler 테스트 수정

--- test/testcase/cgirequesthandler/cgi_program.c
+++ test/testcase/cgirequesthandler/cgi_program.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <memory.h>
 
 int main(int argc, char **argv, char **envp)
 {
@@ -8,26 +7,40 @@ int main(int argc, char **argv, char **envp)
 	char *buff_ptr = buff;
 	int temp = 0;
 
-	memset(buff, 0, 300);
-	while ((temp = read(0, buff_ptr, 100)))
+	while (1)
 	{
+		temp = read(STDIN_FILENO, buff_ptr, 100);
 		if (temp < 0)
 		{
 			perror("scanf");
 			return (1);
 		}
+		if (temp == 0)
+		{
+			buff[buff_ptr - buff] = '\0';
+			break;
+		}
 		buff_ptr += temp;
 	}
-	dprintf(2, "scan done\n");
-	printf("message body: %s\n", buff);
+	/// debug
+	dprintf(2, "\n======== CGI Program ========\n");
+	dprintf(2, "\n- Message Body:\n\t%s\n\n", buff);
+	dprintf(2, "- Meta Variables:\n");
 
-	printf("===== META VARIABLES =====\n");
-	printf("this is CGI Program ...\n");
 	int i = 0;
 	while (envp[i])
-		printf("%s\n", envp[i++]);
+		dprintf(2, "\t%s\n", envp[i++]);
+	fflush(stderr);
+
+	// response message
+	printf("\n======== CGI Program ========\n");
+	printf("\n- Message Body: %s\n", buff);
+	printf("- Meta Variables:\n");
+
+	i = 0;
+	while (envp[i])
+		printf("\t%s\n", envp[i++]);
 
 	fflush(stdout);
-	close(STDOUT_FILENO);
 	return (0);
 }
--- test/testsrcs/test_cgirequesthandler.cpp
+++ test/testsrcs/test_cgirequesthandler.cpp
@@ -9,6 +9,21 @@ int main()
 	try
 	{
 		CGI::Request cgi_request;
+
+		cgi_request.setMessageBody("this is the body man~");
+
+		cgi_request.setMetaVariable("CONTENT_LENGTH", "10");
+		cgi_request.setMetaVariable("CONTENT_TYPE", "text/html");
+		cgi_request.setMetaVariable(
+			"PATH_INFO", "./test/testcase/cgirequesthandler/cgi_program.exe");
+		cgi_request.setMetaVariable(
+			"PATH_TRANSLATED", "/Users/hyeyukim/42cursus/webserv/test/testcase/"
+							   "cgirequesthandler/cgi_program.exe");
+		cgi_request.setMetaVariable("QUERY_STRING", "hi hi");
+		cgi_request.setMetaVariable("REQUEST_METHOD", "GET");
+		cgi_request.setMetaVariable("SERVER_NAME", "localhost");
+		cgi_request.setMetaVariable("SERVER_PORT", "80");
+
 		CGI::RequestHandler cgi_request_handler(cgi_request, 1000);
 		async::Logger::blockingWrite();
 		while (1)
@@ -16,10 +31,10 @@ int main()
 			int rc = cgi_request_handler.task();
 			async::Logger::blockingWrite();
 			sleep(1);
-			
+
 			if (rc == CGI::RequestHandler::CGI_RESPONSE_STATUS_OK)
 				break;
-		    else
+			else
 				continue;
 		}
 	}
