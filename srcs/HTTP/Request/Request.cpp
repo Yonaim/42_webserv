d127e467a78d8d16089e3b884f5bb266093c46cd
Donghyeon Lee <earthicko@gmail.com>
2023-06-01 22:21:24 +0900

    Feat: 로그 메시지 경량화

--- srcs/HTTP/Request/Request.cpp
+++ srcs/HTTP/Request/Request.cpp
@@ -52,22 +52,16 @@ Request &Request::operator=(const Request &orig)
 
 int Request::parse(std::string &buffer, size_t client_max_body_size)
 {
-	// TODO: 디버그 완료 후 하단 변수 삭제
-	const char *state_names[]
-		= {"STARTLINE", "HEADER", "BODY", "CHUNK", "TRAILER"};
-	const char *code_names[] = {"OK", "INVALID", "AGAIN", "IN PROCESS"};
-
 	while (true)
 	{
 		int rc;
-
-		ASYNC_LOG_DEBUG(_logger, __func__ << ": current state is "
-										  << state_names[_current_state]);
+		ASYNC_LOG_DEBUG(_logger,
+						__func__ << ": current state is " << _current_state);
 		switch (_current_state)
 		{
 		case PARSE_STATE_STARTLINE:
 			rc = consumeStartLine(buffer);
-			ASYNC_LOG_DEBUG(_logger, "Got return code " << code_names[rc]);
+			ASYNC_LOG_DEBUG(_logger, "Got return code " << rc);
 
 			if (rc == RETURN_TYPE_OK)
 				_current_state = PARSE_STATE_HEADER;
@@ -77,7 +71,7 @@ int Request::parse(std::string &buffer, size_t client_max_body_size)
 
 		case PARSE_STATE_HEADER:
 			rc = consumeHeader(buffer);
-			ASYNC_LOG_DEBUG(_logger, "Got return code " << code_names[rc]);
+			ASYNC_LOG_DEBUG(_logger, "Got return code " << rc);
 
 			if (rc == RETURN_TYPE_OK)
 			{
@@ -164,12 +158,12 @@ int Request::parse(std::string &buffer, size_t client_max_body_size)
 
 		case PARSE_STATE_BODY:
 			rc = consumeBody(buffer);
-			ASYNC_LOG_DEBUG(_logger, "Got return code " << code_names[rc]);
+			ASYNC_LOG_DEBUG(_logger, "Got return code " << rc);
 			return (rc);
 
 		case PARSE_STATE_CHUNK:
 			rc = consumeChunk(buffer);
-			ASYNC_LOG_DEBUG(_logger, "Got return code " << code_names[rc]);
+			ASYNC_LOG_DEBUG(_logger, "Got return code " << rc);
 			ASYNC_LOG_DEBUG(_logger,
 							"total content length " << _content_length);
 			ASYNC_LOG_DEBUG(_logger,
@@ -194,7 +188,7 @@ int Request::parse(std::string &buffer, size_t client_max_body_size)
 
 		case PARSE_STATE_TRAILER:
 			rc = consumeTrailer(buffer);
-			ASYNC_LOG_DEBUG(_logger, "Got return code " << code_names[rc]);
+			ASYNC_LOG_DEBUG(_logger, "Got return code " << rc);
 			return (rc);
 		}
 	}
