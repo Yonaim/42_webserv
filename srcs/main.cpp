6943f3521da8369eb965c47c6224a26929872033
Donghyeon Lee <earthicko@gmail.com>
2023-06-03 14:41:28 +0900

    Fix: 예외 처리 분기 조정 (G++13 호환성)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
 		config_path = "example.conf";
 	else if (argc == 2)
 		config_path = argv[1];
-	if (argc > 2)
+	else
 	{
 		std::cout << "usage: " << argv[0] << " path/to/config/file\n";
 		return (2);
